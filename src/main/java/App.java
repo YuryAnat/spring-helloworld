d9350027ef1e6098a1c76ba0915c5423b11b3377
User <anatienko.yv@gmail.com>
2020-12-08 22:26:38 +0300

    spring test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        
         HelloWorld hello1 = applicationContext.getBean("helloworld", HelloWorld.class);
         HelloWorld hello2 = applicationContext.getBean("helloworld", HelloWorld.class);
 
